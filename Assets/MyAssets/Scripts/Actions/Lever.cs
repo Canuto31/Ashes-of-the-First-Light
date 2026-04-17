6ed5f28378e602ac8ed3c28606b112c71343a840
canuto31 <jcmt.personal.vg@gmail.com>
2026-04-16 21:56:19 -0500

    Add logic to manage requirement to use the lever

--- Assets/MyAssets/Scripts/Actions/Lever.cs
+++ Assets/MyAssets/Scripts/Actions/Lever.cs
@@ -11,18 +11,40 @@ public class Lever : MonoBehaviour, IInteractable
     [Header("Door")]
     [SerializeField] private DoorController _targetDoor;
 
+    [Header("Requirements")]
+    [SerializeField] private bool _requiresKey = true;
+    [SerializeField] private string _missingRequirementMessage = "It's loocked...";
+
+    [SerializeField] private bool _isUnlocked = false;
+    private bool _hasBeenUsed = false;
+
     public string GetInteractionText()
     {
+        if (_hasBeenUsed) return "";
+
+        if (!_isUnlocked) return "Locked";
+
         return "Press E to interact";
     }
 
     public void Interact()
     {
+        if (_hasBeenUsed) return;
+
+        if (!_isUnlocked) {
+            InteractionUIManager.Instance.Show(transform.parent, _missingRequirementMessage);
+            return;
+        }
+
         StartCoroutine(LeverSequence());
     }
 
     private IEnumerator LeverSequence()
     {
+        _hasBeenUsed = true;
+
+        //GetComponent<Collider2D>().enabled = false;
+
         CameraDirector.Instance.FocusOn(_targetCamera, _cameraDuration);
 
         yield return new WaitForSeconds(1f);
@@ -30,6 +52,11 @@ public class Lever : MonoBehaviour, IInteractable
         if (_targetDoor != null)
         {
             _targetDoor.Open();
+            InteractionUIManager.Instance.Hide();
         }
+    }
+
+    public void Unlock() {
+        _isUnlocked = true;
     }
 }
